4c50c9f26481001fcb65c2f9e716382e0f2450a1
maxim920 <90177561+maxim920@users.noreply.github.com>
2021-11-28 15:58:00 +0200

    Add files via upload

---
+++ zavd3.py
@@ -0,0 +1,46 @@
+import pylab
+
+filePath = "zavd3.txt"
+file1 = open(filePath, "r", encoding='UTF-8').read().lower().split()
+
+symbols = []
+
+for word in file1:
+    for symbol in word:
+        symbols.append(symbol)  
+
+symbols.sort()
+
+example = [".","!","?"] 
+sentences =[]
+
+for symbol1 in symbols:
+    if symbol1 in example:
+        sentences.append(symbol1)
+
+frequency = {}
+
+for item in sentences:
+   if item in frequency:
+        frequency[item] += 1
+   else:
+        frequency[item] = 1
+
+
+keys, values = zip(*frequency.items())
+
+try:
+    print("Розповідні речення: ",values[1])
+except:
+    print("Немає розповідних речень")
+try:
+    print("Питальні речення: ",values[0])
+except:
+    print("Немає питальних речень")
+try:
+    print("Окличні речення: ", values[2])
+except:
+    print("Немає окличних речень")
+
+pylab.bar(keys, values)
+pylab.show()
